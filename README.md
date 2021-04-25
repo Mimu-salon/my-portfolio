b0c4110bb715ae93a235dea1764187e8af6953cd
Mimu-salon <tmimura0@gmail.com>
2021-04-25 14:07:42 +0900

    ✨ ヒーローイメージの作成

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ React, Typescript, Chakra UIを使用
 
 ＜リファクタリング予定＞
 ・ヘッダー：HomeヘージとCareerDatailページでハンバーガーメニュー有無でheightが変わっている
+・ヒーローイメージ：フォルダ内のjpegが使用できない
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Box, Flex, Heading } from '@chakra-ui/layout';
 import { memo, VFC } from 'react';
 import { HeaderHome } from '../organisms/layout/header/HeaderHome';
 
@@ -5,7 +6,20 @@ export const Home: VFC = memo(() => {
   return (
     <>
       <HeaderHome />
-      <p>Homeページです</p>
+      
+      <Flex
+        align='center'
+        justify='center'
+        h={{ base: '400px', md: '600px' }}
+        bgImage="url('https://source.unsplash.com/random')"
+        bgSize='cover'
+        bgRepeat='no-repeat'>
+        <Box>
+          <Heading as='h1' color='white' fontSize={{ base: '3xl', md: '5xl' }}>
+            Takeshi Mimura's Portfolio
+          </Heading>
+        </Box>
+      </Flex>
     </>
   );
 });
