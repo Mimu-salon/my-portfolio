5943e2cbd57086434981c8beb93bdbc77d08cfe3
Mimu-salon <tmimura0@gmail.com>
2021-05-09 18:38:14 +0900

    ⚡️ homeフォルダ名の修正

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,10 +1,10 @@
 import { memo, VFC } from 'react';
 
 import { HeaderHome } from '../organisms/layout/header/HeaderHome';
-import { Profile } from '../organisms/Home/Profile';
-import { HeroImage } from '../organisms/Home/HeroImage';
-import { Carrer } from '../organisms/Home/Career';
-import { SkillsPortfolio } from '../organisms/Home/SkillsPortfolio';
+import { Profile } from '../organisms/home/Profile';
+import { HeroImage } from '../organisms/home/HeroImage';
+import { Carrer } from '../organisms/home/Career';
+import { SkillsPortfolio } from '../organisms/home/SkillsPortfolio';
 import { Footer } from '../organisms/layout/footer/Footer';
 
 export const Home: VFC = memo(() => {
